b227d60768e89f04de41ca2544842281fec34d2f
Lucas Caribé <lucasmoc47@gmail.com>
2021-07-23 19:43:24 -0300

    Finalizando

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,31 +1,63 @@
 import React from 'react';
-import pokemonList from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor() {
-      super();
+  constructor(props) {
+    super();
 
-      this.state = {
-        currentPoke: 0,
-      }
-      this.handleClick = this.handleClick.bind(this)
-    }
+    this.state = {
+      filteredPokemon: props.pokemonList,
+      currentPoke: 0,
+    };
 
-    handleClick () {
-      const nextPoke = this.state.currentPoke === pokemonList.length - 1 ? 0 : this.state.currentPoke + 1;
-      this.setState({
-        currentPoke: nextPoke,
-      })
-    }
+    this.handlePsychic = this.handlePsychic.bind(this);
+    this.handleFire = this.handleFire.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleFire() {
+    const newPokemonList = this.props.pokemonList.filter(
+      (pokemon) => pokemon.type === 'Fire',
+    );
+
+    this.setState({
+      filteredPokemon: newPokemonList,
+      currentPoke: 0,
+    });
+  }
+
+  handlePsychic() {
+    const newPokemonList = this.props.pokemonList.filter(
+      (pokemon) => pokemon.type === 'Psychic',
+    );
+
+    this.setState({
+      filteredPokemon: newPokemonList,
+      currentPoke: 0,
+    });
+  }
+
+  handleClick() {
+    let nextPokemon = this.state.currentPoke + 1;
+
+    if (nextPokemon >= this.state.filteredPokemon.length - 1) nextPokemon = 0;
+
+    this.setState({
+      filteredPokemon: this.state.filteredPokemon,
+      currentPoke: nextPokemon,
+    });
+  }
 
   render() {
-    const { pokemonList } = this.props;
-
     return (
       <div className="pokedex">
-        <Pokemon key={pokemonList[this.state.currentPoke].id} pokemon={pokemonList[this.state.currentPoke]} />
-        <button onClick= { this.handleClick } >Next</button>
+        <Pokemon
+          key={this.state.filteredPokemon[this.state.currentPoke].id}
+          pokemon={this.state.filteredPokemon[this.state.currentPoke]}
+        />
+        <button onClick={this.handleFire}>Fire</button>
+        <button onClick={this.handlePsychic}>Psychic</button>
+        <button onClick={this.handleClick}>Next</button>
       </div>
     );
   }
