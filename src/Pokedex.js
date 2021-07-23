811046dcd3ba9d2c33b18b707dae12e6818b7f5d
bvottri <bruna.votre@gmail.com>
2021-07-23 19:02:59 -0300

    trocando piloto

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pokemonList from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
@@ -8,13 +9,23 @@ class Pokedex extends React.Component {
       this.state = {
         currentPoke: 0,
       }
+      this.handleClick = this.handleClick.bind(this)
     }
+
+    handleClick () {
+      // const nextPoke = this.state.currentPoke === pokemonList
+      this.setState({
+        currentPoke: this.state.currentPoke + 1,
+      })
+    }
+
   render() {
     const { pokemonList } = this.props;
 
     return (
       <div className="pokedex">
         <Pokemon key={pokemonList[this.state.currentPoke].id} pokemon={pokemonList[this.state.currentPoke]} />
+        <button onClick= {handleClick} >Next</button>
       </div>
     );
   }
