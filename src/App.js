56767e478e4b034204ccd34dd8fa848c27d67e70
Lucas Caribé <lucasmoc47@gmail.com>
2021-07-23 18:40:27 -0300

    Trocando o piloto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './App.css';
-import pokemons from './data';
+
 import Pokedex from './Pokedex';
+import Button from './Button';
+
+import pokemons from './data';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
+      <Button />
     </div>
   );
 }
